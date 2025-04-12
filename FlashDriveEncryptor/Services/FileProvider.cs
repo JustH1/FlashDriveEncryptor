821738a1ddcd189860d839c8a9d325a89dacee19
JustH1 <lukin.sergey.01@mail.ru>
2025-04-12 22:13:51 +0300

    Initial commit

---
+++ FlashDriveEncryptor/Services/FileProvider.cs
@@ -0,0 +1,22 @@
+﻿namespace FlashDriveEncryptor.Services
+{
+    internal class FileProvider
+    {
+        private readonly ILogger _logger;
+        private readonly string _diskPath;
+
+        public FileProvider(ILogger logger)
+        {
+            _logger = logger;
+            _diskPath = GetDiskPath();
+        }
+        public string[] GetFilesEncryption()
+        {
+            return Directory.GetFiles(_diskPath, ".", SearchOption.AllDirectories);
+        }
+        private string GetDiskPath()
+        {
+            return Path.GetPathRoot(Directory.GetCurrentDirectory());
+        }
+    }
+}
